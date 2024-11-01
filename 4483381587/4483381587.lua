934460e906df9f8a6f48dd8d9c78b4f30e14fc6c
mki169 <162124491+mki169@users.noreply.github.com>
2024-11-01 21:27:25 +0800

    Delete 4483381587/4483381587.lua

--- 4483381587/4483381587.lua
+++
@@ -1,6 +0,0 @@
---[[
-THE .LUA SCRIPT IS FOR TESTING PURPOSE USEAGE. PLEASE DO NOT RUN IT.
-(It clearly does nothing useful if you know basic luau.)
-]]--
-
-print("Test passed" .. "You're in game: " .. PlaceID)
