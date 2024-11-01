f3d903ccd1ba6ac25ec48275dbed22d3ae627818
mki169 <162124491+mki169@users.noreply.github.com>
2024-11-01 19:27:53 +0800

    Create 4483381587.lua

---
+++ 4483381587/4483381587.lua
@@ -0,0 +1,6 @@
+--[[
+THE .LUA SCRIPT IS FOR TESTING PURPOSE USEAGE. PLEASE DO NOT RUN IT.
+(It clearly does nothing useful if you know basic luau.)
+]]--
+
+print("Test passed" .. "You're in game: " .. GameID)
