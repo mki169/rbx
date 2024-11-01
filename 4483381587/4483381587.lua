be537ccee4b36a670a80eb22569ba55e51793a5e
mki169 <162124491+mki169@users.noreply.github.com>
2024-11-01 19:42:41 +0800

    Update 4483381587.lua

--- 4483381587/4483381587.lua
+++ 4483381587/4483381587.lua
@@ -3,4 +3,4 @@ THE .LUA SCRIPT IS FOR TESTING PURPOSE USEAGE. PLEASE DO NOT RUN IT.
 (It clearly does nothing useful if you know basic luau.)
 ]]--
 
-print("Test passed" .. "You're in game: " .. GameID)
+print("Test passed" .. "You're in game: " .. PlaceID)
